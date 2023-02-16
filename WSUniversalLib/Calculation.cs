493da9b3112b491429f483c0c411080ec05b42e0
ФасхиеваДР <@101-11>
2023-02-16 10:51:07 +0300

    Добавьте файлы проекта.

---
+++ WSUniversalLib/Calculation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSUniversalLib
+{
+    public class Calculation
+    {
+        public static int GetQuantityForProduct(int productType, int materialType, int count, float width, float length)
+        {
+            if (count < 0)
+            {
+                return -1;
+            }
+            double rez = width * length * count;
+            if (rez < 0)
+            {
+                return -1;
+            }
+            switch (productType) //коэффициент типа продукции
+            {
+                case 1:
+                    rez = rez * 1.1;
+                    break;
+                case 2:
+                    rez = rez * 2.5;
+                    break;
+                case 3:
+                    rez = rez * 8.43;
+                    break;
+                    default:
+                    return -1;
+            }
+            switch (materialType) //учитывание % брака материала в зависимости от типа
+            {
+                case 1:
+                    rez += (rez / 100) * 0.3;
+                    break;
+                case 2:
+                    rez += (rez / 100) * 0.12;
+                    break;
+                default:
+                    return -1;
+            }
+            return (int)Math.Round(rez); //округление результата
+        }
+    }
+}
